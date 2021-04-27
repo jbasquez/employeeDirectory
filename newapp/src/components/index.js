184e50ac7f5f858ff15b5337f6950696618e2134
Joe Basquez <jybasquez@yahoo.com>
2021-04-27 18:20:03 -0400

    render and return set up

--- newapp/src/components/index.js
+++ newapp/src/components/index.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 
 class Form extends Component {
-
+    render() {
+        return (
+            <div>
+                <p>test123</p>
+            </div>
+        )
+    }
 };
 
 export default Form;
